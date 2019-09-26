3e46224f9a6e2c8cc423ee34d4825119f0368891
amelow <amelielowe96@gmail.com>
2019-09-26 14:36:24 +0200

    getting the methods from the interface

--- Assignment1/src/Exercise1/MyIntegerBST.java
+++ Assignment1/src/Exercise1/MyIntegerBST.java
@@ -1,6 +1,24 @@
 package Exercise1;
 
-public class MyIntegerBST {
+public class MyIntegerBST implements A1Tree {
+
+	@Override
+	public void insert(Integer value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Integer mostSimilarValue(Integer value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printByLevels() {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 
