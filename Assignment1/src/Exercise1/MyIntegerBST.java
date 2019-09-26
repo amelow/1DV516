49593f73e780c5f5440bd9eb8aa35b7d462a1608
amelow <amelielowe96@gmail.com>
2019-09-26 15:00:52 +0200

    private node class added

--- Assignment1/src/Exercise1/MyIntegerBST.java
+++ Assignment1/src/Exercise1/MyIntegerBST.java
@@ -2,24 +2,31 @@ package Exercise1;
 
 public class MyIntegerBST implements A1Tree {
 
+	// MyIntegerBST BST = new MyIntegerBST();
+	private Node root;
+	private int size;
+
 	@Override
 	public void insert(Integer value) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Integer mostSimilarValue(Integer value) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public void printByLevels() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
-
+	private class Node {
+		private int value;
+		private int size;
+		private Node left;
+		private Node right;
+	}
 
 }
