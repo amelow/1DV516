5a4e3cd3170a5d484e485f469a5773d8569d8892
amelow <amelielowe96@gmail.com>
2019-09-28 13:43:27 +0200

    add unimplemented methods

--- Assignment1/src/Exercise2/A1SequenceWithMinimum.java
+++ Assignment1/src/Exercise2/A1SequenceWithMinimum.java
@@ -1,11 +1,14 @@
-package assignment1AADS.assignment1;
-
-
+package Exercise2;
 
 public interface A1SequenceWithMinimum {
-        public void insertRight(Integer value); 
+	
+	public void insertRight(Integer value);
+
 	public Integer removeRight();
+
 	public void insertLeft(Integer value);
+
 	public Integer removeLeft();
+
 	public Integer findMinimum();
 }
---
+++ Assignment1/src/Exercise2/MyADT.java
@@ -0,0 +1,35 @@
+package Exercise2;
+
+public class MyADT implements A1SequenceWithMinimum {
+
+	@Override
+	public void insertRight(Integer value) {
+		
+		
+	}
+
+	@Override
+	public Integer removeRight() {
+	
+		return null;
+	}
+
+	@Override
+	public void insertLeft(Integer value) {
+		
+		
+	}
+
+	@Override
+	public Integer removeLeft() {
+		
+		return null;
+	}
+
+	@Override
+	public Integer findMinimum() {
+		
+		return null;
+	}
+
+}
