a773bdcf1985f0ecd9287bbd334a20fc8761080f
amelow <amelielowe96@gmail.com>
2019-09-28 15:02:12 +0200

    update on insert right and new method toString

--- Assignment1/src/Exercise2/MySequenceWithMinimum.java
+++ Assignment1/src/Exercise2/MySequenceWithMinimum.java
@@ -3,8 +3,7 @@ package Exercise2;
 public class MySequenceWithMinimum implements A1SequenceWithMinimum {
 	private Node head = null;
 	private Node tail = null;
-	private int next;
-	private int prev;
+	private Node temp = null;
 
 	public MySequenceWithMinimum() {
 
@@ -12,14 +11,15 @@ public class MySequenceWithMinimum implements A1SequenceWithMinimum {
 
 	@Override
 	public void insertRight(Integer value) {
-		if (head == null) {
-			head = new Node(value);
-			  tail = null;
-		} else if(tail== null){
-			  tail = new Node(value);
-			System.out.println("hej"+ value);
-
+		if (head == null && tail == null) {
+			temp = new Node(value);
+			head = temp;
+			tail = temp;
+		} else {
+			tail.next = new Node(value);
+			tail = tail.next;
 		}
+		System.out.print(value + ",");
 
 	}
 
@@ -46,10 +46,15 @@ public class MySequenceWithMinimum implements A1SequenceWithMinimum {
 		return null;
 	}
 
+	public String toString() {
+		return null;
+
+	}
+
 	public class Node {
 		Integer value;
-		Node left = null;
-		Node right = null;
+		Node next = null;
+		Node prev = null;
 
 		Node(int v) {
 			value = v;
--- Assignment1/src/Exercise2/SequenceMain.java
+++ Assignment1/src/Exercise2/SequenceMain.java
@@ -3,10 +3,18 @@ package Exercise2;
 public class SequenceMain {
 
 	public static void main(String[] args) {
-		 MySequenceWithMinimum seq = new MySequenceWithMinimum();
-		 seq.insertRight(5);
-		 seq.insertRight(4);
-		 seq.insertRight(15);
-	}
+		MySequenceWithMinimum seq = new MySequenceWithMinimum();
 
+		System.out.println("Expected Output of insertRight() method: 5,4,15");
+		System.out.println("- Output of insertRight() method - " + "\n");
+		seq.insertRight(5);
+		seq.insertRight(4);
+		seq.insertRight(15);
+		System.out.print(seq.toString());
+
+		/*
+		 * System.out.println("Expected Output of removeRight() method: 5,4,15");
+		 * System.out.println("- Output of removeRight() method - " + "\n");
+		 */
+	}
 }
