fe8cd4b68c5a3826f94bbaff5f3439fb7f46d16e
amelow <amelielowe96@gmail.com>
2019-09-28 13:48:45 +0200

    added unimplemented methods

--- Assignment1/src/Exercise2/MySequenceWithMinimum.java
+++ Assignment1/src/Exercise2/MySequenceWithMinimum.java
@@ -4,31 +4,29 @@ public class MySequenceWithMinimum implements A1SequenceWithMinimum {
 
 	@Override
 	public void insertRight(Integer value) {
-		
-		
+
 	}
 
 	@Override
 	public Integer removeRight() {
-	
+
 		return null;
 	}
 
 	@Override
 	public void insertLeft(Integer value) {
-		
-		
+
 	}
 
 	@Override
 	public Integer removeLeft() {
-		
+
 		return null;
 	}
 
 	@Override
 	public Integer findMinimum() {
-		
+
 		return null;
 	}
 
