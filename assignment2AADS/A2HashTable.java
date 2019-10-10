99cb703a1c1be11a9a6d41cde703fbfe4e36e356
amelow <amelielowe96@gmail.com>
2019-10-10 12:49:50 +0200

    structure of assignment

---
+++ assignment2AADS/A2HashTable.java
@@ -0,0 +1,12 @@
+package assignment2AADS.assignment2;
+
+public interface A2HashTable<T> {
+
+    public void insert(T element);
+    
+    public void delete(T element);
+
+    public boolean contains(T element);
+    
+    public int getLengthOfArray();
+}
