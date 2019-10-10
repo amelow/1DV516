f0afaa3b2b3be0c6f40b38f5aa19b50875b426e8
amelow <amelielowe96@gmail.com>
2019-10-10 12:54:15 +0200

    removing TODO comments

--- Assignment2/src/Exercise1/MyHashTable.java
+++ Assignment2/src/Exercise1/MyHashTable.java
@@ -4,25 +4,25 @@ public class MyHashTable implements A2HashTable {
 
 	@Override
 	public void insert(Object element) {
-		// TODO Auto-generated method stub
+	
 		
 	}
 
 	@Override
 	public void delete(Object element) {
-		// TODO Auto-generated method stub
+	
 		
 	}
 
 	@Override
 	public boolean contains(Object element) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	public int getLengthOfArray() {
-		// TODO Auto-generated method stub
+	
 		return 0;
 	}
 
