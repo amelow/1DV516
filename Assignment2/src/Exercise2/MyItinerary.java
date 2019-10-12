c2fd791548d2c4307cbc964e468df1e730070065
amelow <amelielowe96@gmail.com>
2019-10-12 11:19:06 +0200

    adding the example file from the teacher

--- Assignment2/src/Exercise2/MyItinerary.java
+++ Assignment2/src/Exercise2/MyItinerary.java
@@ -4,25 +4,25 @@ public class MyItinerary implements A2Itinerary<A2Direction> {
 
 	@Override
 	public A2Direction[] rotateRight() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public int widthOfItinerary() {
-		// TODO Auto-generated method stub
+
 		return 0;
 	}
 
 	@Override
 	public int heightOfItinerary() {
-		// TODO Auto-generated method stub
+
 		return 0;
 	}
 
 	@Override
 	public int[] getIntersections() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
