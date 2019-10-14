a7f93b06674673c457e7eab9ef22368d57feb926
amelow <amelielowe96@gmail.com>
2019-10-14 23:43:43 +0200

    added the A2Measure interface

--- Assignment2/src/Exercise3/MeasureMain.java
+++ Assignment2/src/Exercise3/MeasureMain.java
@@ -1,11 +1,11 @@
 package Exercise3;
+
 /*
  * Authors: Håkan Johansson, Amelie Löwe.
  */
 public class MeasureMain {
 
 	public static void main(String[] args) {
-		
 
 	}
 
--- Assignment2/src/Exercise3/MyMeasure.java
+++ Assignment2/src/Exercise3/MyMeasure.java
@@ -1,7 +1,26 @@
 package Exercise3;
+
 /*
  * Authors: Håkan Johansson, Amelie Löwe.
  */
-public class MyMeasure {
+public class MyMeasure implements A2Measure {
+
+	@Override
+	public boolean isSameCollection(int[] array1, int[] array2) {
+
+		return false;
+	}
+
+	@Override
+	public int minDifferences(int[] array1, int[] array2) {
+
+		return 0;
+	}
+
+	@Override
+	public int[] getPercentileRange(int[] arr, int lower, int upper) {
+
+		return null;
+	}
 
 }
