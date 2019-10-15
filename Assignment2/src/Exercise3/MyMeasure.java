0635be64f4b2b085135526f015bc3a14155a7d18
amelow <amelielowe96@gmail.com>
2019-10-15 10:38:55 +0200

    removing some spaces

--- Assignment2/src/Exercise3/MyMeasure.java
+++ Assignment2/src/Exercise3/MyMeasure.java
@@ -10,13 +10,11 @@ public class MyMeasure implements A2Measure {
 
 		return false;
 	}
-
 	@Override
 	public int minDifferences(int[] array1, int[] array2) {
 
 		return 0;
 	}
-
 	@Override
 	public int[] getPercentileRange(int[] arr, int lower, int upper) {
 
