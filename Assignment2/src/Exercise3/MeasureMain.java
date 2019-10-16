32f0d7d1c5c78831045693b6946feae5045a9f69
amelow <amelielowe96@gmail.com>
2019-10-16 13:49:54 +0200

    isSameCollection() in exercise3

--- Assignment2/src/Exercise3/MeasureMain.java
+++ Assignment2/src/Exercise3/MeasureMain.java
@@ -4,9 +4,14 @@ package Exercise3;
  * Authors: Håkan Johansson, Amelie Löwe.
  */
 public class MeasureMain {
-
 	public static void main(String[] args) {
+		MyMeasure measure = new MyMeasure();
+		int array1[] = { 3, 5, 2, 5, 2 };
+		int array2[] = { 2, 3, 5, 5, 2 };
 
+		if (measure.isSameCollection(array1, array2))
+			System.out.println("Yes");
+		else
+			System.out.println("No");
 	}
-
 }
--- Assignment2/src/Exercise3/MyMeasure.java
+++ Assignment2/src/Exercise3/MyMeasure.java
@@ -7,14 +7,19 @@ public class MyMeasure implements A2Measure {
 
 	@Override
 	public boolean isSameCollection(int[] array1, int[] array2) {
+		if (array1.length != array2.length) {
+			return false;
+		}
 
 		return false;
 	}
+
 	@Override
 	public int minDifferences(int[] array1, int[] array2) {
 
 		return 0;
 	}
+
 	@Override
 	public int[] getPercentileRange(int[] arr, int lower, int upper) {
 
