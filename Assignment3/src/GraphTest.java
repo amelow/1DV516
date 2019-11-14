bf31e2d206448e90f319a7576c49c8729bf7c5a8
amelow <amelielowe96@gmail.com>
2019-11-14 10:28:39 +0100

    comments in graphTest class done

--- Assignment3/src/GraphTest.java
+++ Assignment3/src/GraphTest.java
@@ -36,6 +36,8 @@ public class GraphTest {
 	private static void TestDirectedGraph() {
 		System.out.println("\n" + "---------- DIRECTEDGRAPH ----------" + "\n");
 		MyDirectedGraph directedGraph = new MyDirectedGraph(5);
+
+		// Adding edges between the vertices in the graph
 		directedGraph.addEdge(0, 1);
 		directedGraph.addEdge(1, 2);
 		directedGraph.addEdge(0, 3);
@@ -43,7 +45,6 @@ public class GraphTest {
 		directedGraph.addEdge(2, 3);
 		directedGraph.addEdge(4, 3);
 		directedGraph.addEdge(4, 0);
-		// directedGraph.addEdge(4, 4);
 
 		// Checking if the directed graph is acyclic
 		System.out.println("Is Acyclic: " + directedGraph.isAcyclic());
@@ -58,6 +59,8 @@ public class GraphTest {
 	private static void TestSocialNetwork() {
 		System.out.println("\n" + "---------- MYSOCIALNETWORK ----------" + "\n");
 		MySocialNetwork network = new MySocialNetwork(10);
+
+		// Adding edges between the vertices in the graph
 		network.addEdge(0, 1);
 		network.addEdge(0, 2);
 		network.addEdge(0, 3);
@@ -70,8 +73,13 @@ public class GraphTest {
 		network.addEdge(4, 8);
 		network.addEdge(4, 9);
 
+		// Printing the friends of my friends
 		System.out.println("Number Of People: " + network.numberOfPeopleAtFriendshipDistance(0, 2));
+
+		// The furthest friend of my friends
 		System.out.println("Furthest Distance : " + network.furthestDistanceInFriendshipRelationships(1));
+
+		// Checking out the friends of my friends(if we have any common friends)
 		System.out.println("Possible friends of 0: " + network.possibleFriends(0));
 	}
 }
