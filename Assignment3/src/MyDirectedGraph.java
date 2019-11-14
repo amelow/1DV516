80b1a183b4fb618a814da9aee177590f3a075f19
amelow <amelielowe96@gmail.com>
2019-11-14 23:58:55 +0100

    update

--- Assignment3/src/MyDirectedGraph.java
+++ Assignment3/src/MyDirectedGraph.java
@@ -1,193 +1,343 @@
-
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
-
+ 
 /*
  * Authors: Håkan Johansson and Amelie Löwe for the 1DV516 course
  */
 public class MyDirectedGraph implements A3Graph {
-	private static List<ArrayList<Integer>> adjacency;
-	private int numOfVertices;
-
-	/*
-	 * Constructor which loops thru the amount of vertices and calls upon the add
-	 * function. The vertices will always start from zero.
-	 */
-	MyDirectedGraph(int amountOfVertices) {
-		numOfVertices = amountOfVertices;
-		adjacency = new ArrayList<ArrayList<Integer>>();
-
-		for (int i = 0; i < amountOfVertices; i++) {
-			adjacency.add(new ArrayList<Integer>());
-		}
-	}
-
-	// Getter and Setter functions
-
-	public void setNumOfVertices(int numOfVertices) {
-		this.numOfVertices = numOfVertices;
-	}
-
-	public int getNumOfVertices() {
-		return numOfVertices;
-	}
-
-	public List<ArrayList<Integer>> getAdjacency() {
-		return adjacency;
-	}
-
-	@Override
-	public void addVertex(int vertex) {
-		adjacency.add(new ArrayList<Integer>());
-		numOfVertices++;
-	}
-
-	/*
-	 * Adds only a edge from source to target to set an direction
-	 */
-	@Override
-	public void addEdge(int sourceVertex, int targetVertex) {
-		adjacency.get(sourceVertex).add(targetVertex);
-
-	}
-
-	/*
-	 * Checks if the graph is connected by using a Depth first search method called
-	 * connectionDFS
-	 */
-	@Override
-	public boolean isConnected() {
-		boolean[] visited = new boolean[numOfVertices];
-		Stack<Integer> s = new Stack<Integer>();
-
-		for (int i = 0; i < numOfVertices; i++) {
-			if (!visited[i] && adjacency.get(i).size() != 0) {
-				connectionDFS(i, visited, s);
-			}
-
-		}
-		return s.size() == numOfVertices;
-	}
-
-	/*
-	 * Recursive help method for traversing thru the graph
-	 */
-	public void connectionDFS(int pos, boolean[] isVisited, Stack<Integer> s) {
-		isVisited[pos] = true;
-
-		for (int i = 0; i < adjacency.get(pos).size(); i++) {
-			if (!isVisited[pos]) {
-				connectionDFS(i, isVisited, s);
-			}
-
-		}
-		s.push(pos);
-
-	}
-
-	/*
-	 * Traverses thru the graph by using the isAcyclicDFS() method and returns true
-	 * if the graph is acyclic.
-	 */
-	@Override
-	public boolean isAcyclic() {
-		Stack<Integer> stack = new Stack<Integer>();
-		boolean[] isVisited = new boolean[numOfVertices];
-		int n = 0;
-
-		while (n < isVisited.length) {
-			if (!isVisited[n]) // checks which are set to true(aka visited) or false
-				if (!isAcyclicDFS(n, isVisited, stack)) // recursive help method
-					return false;
-			n++;
-		}
-
-		return true;
-	}
-
-	/*
-	 * Recursive help method for traversing thru the graph
-	 */
-	public static boolean isAcyclicDFS(int n, boolean[] isVisited, Stack stack) {
-		if (stack.contains(n))
-			return false;
-		isVisited[n] = true;
-		stack.push(n);
-
-		for (int curr : adjacency.get(n)) {
-			if (n == curr) // if they get back to the same vertice by taking another way it must be cyclic
-				return false;
-			if (isVisited[curr])
-				continue;
-			if (!isAcyclicDFS(curr, isVisited, stack))
-				return false;
-		}
-		stack.pop();
-		return true;
-	}
-
-	/*
-	 * Returns a List of Lists of integers (List<List<Integer>>
-	 * connectedComponents()) that returns the nodes in each connected component of
-	 * the graph
-	 * 
-	 */
-	@Override
-	public List<List<Integer>> connectedComponents() {
-
-		boolean[] isVisited = new boolean[numOfVertices];
-		List<Integer> list = componentsDFS(numOfVertices, isVisited);
-
-		List<List<Integer>> connections = new ArrayList<List<Integer>>();
-
-		for (int i = 0; i < numOfVertices; i++) {
-			if (!isVisited[i]) {
-				connectionDFS(i, isVisited, list);
-			}
-		}
-
-		for (int i = 0; i < isVisited.length; i++) {
-			isVisited[i] = false;
-		}
-		for (int i = 0; i < list.size(); i++) {
-			int n = list.get(i);
-			if (!isVisited[n]) {
-				List<Integer> temp = new ArrayList<Integer>();
-				connectionDFS(n, isVisited, temp);
-				connections.add(temp);
-			}
-
-		}
-		return connections;
-
-	}
-
-	public List<Integer> componentsDFS(int vertexIndex, boolean[] isVisited) {
-		List<Integer> list = new ArrayList<Integer>();
-		for (int i = 0; i < adjacency.get(vertexIndex).size(); i++) {
-			int temp = adjacency.get(vertexIndex).get(i);
-			if (!isVisited[temp]) {
-				connectionDFS(temp, isVisited, list);
-
-			}
-
-		}
-
-		return list;
-	}
-
-	public void connectionDFS(int pos, boolean[] isVisited, List<Integer> list) {
-		isVisited[pos] = true;
-
-		for (int i = 0; i < adjacency.get(pos).size(); i++) {
-			if (!isVisited[pos]) {
-				connectionDFS(i, isVisited, list);
-			}
-
-		}
-
-		list.add(pos);
-
-	}
+    private static List<List<Integer>> adjacency;
+    private int numOfVertices;
+ 
+    MyDirectedGraph(int amountOfVertices) {
+        numOfVertices = amountOfVertices;
+        adjacency = new ArrayList<List<Integer>>();
+ 
+        for (int i = 0; i < amountOfVertices; i++) {
+            adjacency.add(new LinkedList<Integer>());
+        }
+    }
+ 
+    public List<List<Integer>> getAdjacency() {
+        //      System.out.println(adjacency.toString());
+        return adjacency;
+    }
+ 
+    @Override
+    public void addVertex(int vertex) {
+        adjacency.add(new LinkedList<Integer>());
+        numOfVertices++;
+ 
+    }
+ 
+    @Override
+    public void addEdge(int sourceVertex, int targetVertex) {
+        adjacency.get(sourceVertex).add(targetVertex);
+ 
+    }
+ 
+    public void addReverseEdge(int sourceVertex, int targetVertex) {
+        adjacency.get(targetVertex).add(sourceVertex);
+ 
+    }
+ 
+    @Override
+    public boolean isConnected() {
+        List<List<Integer>> Components = connectedComponents();
+       
+        /* boolean[] visited = new boolean[numOfVertices];
+        List<Integer> s = new ArrayList<Integer>();
+ 
+        for (int i = 0; i < numOfVertices; i++) {
+            if (!visited[i] && adjacency.get(i).size() != 0) {
+                connectionDFS(i, visited, s);
+            }
+ 
+        }
+ 
+        return s.size() == numOfVertices;
+         */
+        return (Components.size() ==1);
+    }
+ 
+    /*
+     * Reverse the edges of the graph.
+     */
+ 
+    /*
+     * Reverse the edges of the graph.
+     */
+    public List<List<Integer>> reverseGraph() {
+        //      MyDirectedGraph reverse = new MyDirectedGraph(numOfVertices);
+        Stack<List<Integer>> reverse = new Stack<List<Integer>>();
+        List<List<Integer>> matrix = new ArrayList<List<Integer>>();
+        List<List<Integer>> transpose = new ArrayList<List<Integer>>();
+        //      reverse.addEdge(0, targetVertex);
+ 
+        for (int i = 0; i < numOfVertices; i++) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j < numOfVertices; j++) {
+                row.add(0);
+            }
+            matrix.add(row);
+            //          transpose.add(row);
+        }
+ 
+        for (int i = 0; i < numOfVertices; i++) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j < numOfVertices; j++) {
+                row.add(0);
+            }
+            transpose.add(row);
+        }
+ 
+        for (int i = 0; i < numOfVertices; i++) {
+ 
+            for (int j = 0; j < getAdjacency().get(i).size(); j++) {
+                matrix.get(i).set(getAdjacency().get(i).get(j), 1);
+            }
+ 
+        }
+ 
+        System.out.println(matrix.toString());
+ 
+        /*
+         * Transpose the matrix
+         */
+ 
+        for (int i = 0; i < numOfVertices; i++) {
+ 
+            for (int j = 0; j < numOfVertices; j++) {
+                if (matrix.get(i).get(j) == 1) {
+                    transpose.get(j).set(i, 1);
+                }
+ 
+            }
+ 
+        }
+ 
+        for (int i = 0; i < numOfVertices; i++) {
+            reverse.add(new ArrayList<Integer>());
+        }
+ 
+        System.out.println("Hej: " + reverse.toString());
+        System.out.println("Transpose: " + transpose.toString());
+ 
+        for (int i = 0; i < numOfVertices; i++) {
+            for (int j = 0; j < numOfVertices; j++) {
+                if (transpose.get(i).get(j) == 1) {
+                    reverse.get(i).add(j);
+                }
+            }
+        }
+ 
+        /*
+         * Add the transposed edges to the new graph.
+         */
+        //      MyDirectedGraph tGraph = new MyDirectedGraph(numOfVertices);
+        ////        System.out.println("Reverse graph: " + reverse.toString());
+        //
+        //      for (int i = 0; i < numOfVertices; i++) {
+        //
+        //          if (reverse.get(i).size() != 0) {
+        //              for (int j = 0; j < reverse.get(i).size(); j++) {
+        //                  tGraph.addEdge(i, reverse.get(i).get(j));
+        //              }
+        //          }
+        //
+        //      }
+ 
+        //      System.out.println("oooooo " + tGraph.getAdjacency().toString());
+        return reverse;
+ 
+    }
+ 
+    @Override
+    public boolean isAcyclic() {
+        Stack<Integer> stack = new Stack<Integer>();
+ 
+        boolean[] visited = new boolean[numOfVertices];
+        int n = 0;
+        while (n < visited.length) {
+            if (!visited[n])
+                if (!isAcyclicDFS(n, visited, stack))
+                    return false;
+            n++;
+        }
+ 
+        return true;
+    }
+ 
+    public static boolean isAcyclicDFS(int n, boolean[] isVisited, Stack<Integer> stack) {
+        if (stack.contains(n))
+            return false;
+        isVisited[n] = true;
+        stack.push(n);
+        for (int curr : adjacency.get(n)) {
+            if (n == curr)
+                return false;
+            if (isVisited[curr])
+                continue;
+            if (!isAcyclicDFS(curr, isVisited, stack))
+                return false;
+        }
+        stack.pop();
+        return true;
+    }
+ 
+    @Override
+    public List<List<Integer>> connectedComponents() {
+        boolean[] isVisited = new boolean[numOfVertices];
+        System.out.println("hej");
+        List<Integer> list = componentsDFS(numOfVertices-1, isVisited,adjacency);
+ 
+        List<List<Integer>> connections = new ArrayList<List<Integer>>();
+ 
+        //      List<Integer> temp = new ArrayList<Integer>();
+        //
+        //      List<List<Integer>> connections = new ArrayList<List<Integer>>();
+        ////        System.out.println(connectionDFS(numOfVertices, isVisited, temp));
+        //     
+        //
+ 
+        Collections.reverse(list);
+        for (int i = 0; i < numOfVertices; i++) {
+            if (!isVisited[i]) {
+                connectionDFS(i, isVisited, list, adjacency);
+            }
+        }
+        //
+        ////        System.out.println("Stack components: " + s.toString());
+        List<List<Integer>> reverseEdges = reverseGraph();
+        //
+ 
+ 
+        for (int i = 0; i < isVisited.length; i++) {
+            isVisited[i] = false;
+        }
+ 
+        List<List<Integer>> reverseconnections = new ArrayList<List<Integer>>();
+        //
+        ////        List<Integer> temp = new ArrayList<Integer>();
+        System.out.println("uuuuuuuuu " + list.toString());
+        for (int i = 0; i < list.size(); i++) {
+ 
+            int n = list.get(i);
+ 
+            if (!isVisited[n]) {
+                List<Integer> temp = new ArrayList<Integer>();
+                connectionDFS(n, isVisited, temp,adjacency);
+                connections.add(temp);
+            }
+ 
+        }
+ 
+ 
+        isVisited = new boolean[numOfVertices];
+        for (int i = 0; i < list.size(); i++) {
+ 
+            int n = list.get(i);
+ 
+            if (!isVisited[n]) {
+                List<Integer> temp = new ArrayList<Integer>();
+                connectionDFS(n, isVisited, temp, reverseEdges);
+                reverseconnections.add(temp);
+            }
+ 
+        }
+        System.out.println("Ordinary Connections : "+connections);
+        System.out.println("Reverse connections : " +reverseconnections );
+ 
+        ////        connections.add(temp);
+        return FixConnected(connections, reverseconnections);
+    }
+ 
+    public List<List<Integer>> FixConnected (List<List<Integer>> connections, List<List<Integer>> reverseconnections) {
+        List<List<Integer>> fixedList = new ArrayList<List<Integer>>();
+        boolean connectionsIsBigger = false;
+        if(connections.size() >= reverseconnections.size()) {
+            connectionsIsBigger = true;
+        }
+        if (connectionsIsBigger) {
+            for(int i= 0; i < connections.size(); i++) {
+                List<Integer> TempList = connections.get(i);
+                List<Integer> TempList2 = null;
+                if (i <= reverseconnections.size()-1) {
+                    TempList2 = reverseconnections.get(i);
+                    if(TempList.retainAll(TempList2))
+                        fixedList.add(TempList);
+                        else {
+                            fixedList.add(TempList);
+                        }
+                }
+ 
+            }
+        } else {
+            for(int i= 0; i < reverseconnections.size(); i++) {
+                List<Integer> TempList = reverseconnections.get(i);
+                List<Integer> TempList2 = null;
+                if (i <= connections.size()-1) {
+                    TempList2 = connections.get(i);
+                    if(TempList.retainAll(TempList2))
+                    fixedList.add(TempList);
+                    else {
+                        fixedList.add(TempList);
+                    }
+                }
+ 
+            }
+ 
+ 
+ 
+            System.out.println("This is the fixed List: "+fixedList);
+           
+        }
+        return fixedList;
+    }
+ 
+    public int getNumOfVertices() {
+        return numOfVertices;
+    }
+ 
+    public void setNumOfVertices(int numOfVertices) {
+        this.numOfVertices = numOfVertices;
+    }
+ 
+    public List<Integer> componentsDFS(int vertexIndex, boolean[] isVisited, List<List<Integer>> adjL) {
+        //      isVisited[vertexIndex] = true;
+ 
+        List<Integer> list = new ArrayList<Integer>();
+ 
+        //      for(int i = 0; i < adjacency.get(i).size(); i++) {
+        //          int temp = adjacency.get(vertexIndex).get(i);
+        for (int curr : adjL.get(vertexIndex)) {
+            //          System.out.println("qqqqqqqq " + list.toString());
+            if (!isVisited[curr]) {
+                connectionDFS(curr, isVisited, list, adjL);
+ 
+            }
+ 
+        }
+ 
+        return list;
+    }
+ 
+    public void connectionDFS(int pos, boolean[] isVisited, List<Integer> list, List<List<Integer>> adjL) {
+        isVisited[pos] = true;
+        //      System.out.println("Pos: " + pos);
+        List<Integer> ost = adjL.get(pos);
+        for (int i = 0; i < ost.size(); i++) {
+            if (!isVisited[ost.get(i)]) {
+                System.out.println(ost.get(i)+" Is not visited. Visiting");
+                connectionDFS(ost.get(i), isVisited, list, adjL);
+            }
+ 
+        }
+ 
+        list.add(pos);
+        //      return list;
+    }
+ 
 }
